e60ac9e13bafceca4a8e5220ce9dfaa8ca107457
unknown <manh.tran@pirago.work>
2021-10-16 17:54:16 +0700

    feat: create Articles

---
+++ Service/Split_Articles.py
@@ -0,0 +1,22 @@
+try:
+    f = open('../data/nytimes_news_articles.txt', mode='r', encoding='utf-8');
+    a=f.readlines()
+finally:
+    f.close()
+URL = "URL: http://www.nytimes.com/"
+N_URL = len(URL)
+j=2
+for i in range(1001):
+    namefile = '../data/Articles/nytimes' + str(i) + ".txt"
+    try:
+        f=open(namefile, mode='w', encoding='utf-8')
+        while True:
+            if (a[j][:28] != URL and a[j+1] != "/n"):
+                f.write(a[j])
+                j=j+1
+            else:
+                j=j+2
+                break
+    finally:
+        f.close()
+
